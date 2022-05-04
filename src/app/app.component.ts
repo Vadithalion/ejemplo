aacd4d211d70da48e38dea5a50fde62037f285b0
Iñigo Vadillo <ivadillo6@gmail.com>
2022-05-05 00:02:44 +0200

    header + form first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba';
+
+  constructor (private dialog: MatDialog) {
+
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+  }
 }
